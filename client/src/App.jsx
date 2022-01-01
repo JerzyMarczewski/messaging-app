872e54eddacdff5d8dd7853998269fb8e72848aa
JerzyMarczewski <jerzy.marczewski2@gmail.com>
2022-01-01 23:36:07 +0100

    completed initial Structure

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,23 +1,22 @@
 import React from "react";
-import {StreamChat} from "stream-chat";
+import { StreamChat } from "stream-chat";
 import { Chat } from "stream-chat-react";
 import Cookies from "universal-cookie";
-
-import { ChannelListContainer, ChannelContainer } from "./components";
-
+import { ChannelContainer, ChannelListContainer } from "./components";
 import "./App.css";
 
 const apiKey = `ruyuezx6bs8z`;
 
 const client = StreamChat.getInstance(apiKey);
 
+
 const App = () => {
     return (
         <div className="app__wrapper">
             <Chat client={client} theme="team light">
                 <ChannelListContainer />
-                <ChannelContainer />
-            </Chat>
+                <ChannelContainer />    
+            </Chat> 
         </div>
     )
 }
